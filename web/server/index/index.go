e931f2aae9bcf8b0e6cc7bcb69800f851afad393
Jason Chavannes <jason.chavannes@gmail.com>
2018-07-15 12:26:41 -0700

    Fix new user experience

--- web/server/index/index.go
+++ web/server/index/index.go
@@ -4,6 +4,7 @@ import (
 	"github.com/jchavannes/jgo/jerr"
 	"github.com/jchavannes/jgo/web"
 	"github.com/memocash/memo/app/auth"
+	"github.com/memocash/memo/app/cache"
 	"github.com/memocash/memo/app/db"
 	"github.com/memocash/memo/app/profile"
 	"github.com/memocash/memo/app/res"
@@ -26,6 +27,28 @@ var indexRoute = web.Route{
 		key, err := db.GetKeyForUser(user.Id)
 		if err != nil {
 			r.Error(jerr.Get("error getting key for user", err), http.StatusInternalServerError)
+			return
+		}
+		bal, err := cache.GetBalance(key.PkHash)
+		if err != nil {
+			r.Error(jerr.Get("error getting balance from cache", err), http.StatusInternalServerError)
+			return
+		}
+		r.Helper["Balance"] = bal
+
+		if bal == 0 {
+			pf, err := profile.GetProfile(key.PkHash, key.PkHash)
+			if err != nil {
+				r.Error(jerr.Get("error getting profile for hash", err), http.StatusInternalServerError)
+				return
+			}
+			err = pf.SetQr()
+			if err != nil {
+				r.Error(jerr.Get("error creating qr", err), http.StatusInternalServerError)
+				return
+			}
+			r.Helper["Profile"] = pf
+			r.RenderTemplate(res.TmplDashboardNoFunds)
 			return
 		}
 		r.Helper["Key"] = key
